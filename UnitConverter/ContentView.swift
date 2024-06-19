c247b00e35918a23bdf209e00adf76937867e521
emreeilhan <emreilhn15@gmail.com>
2024-06-19 12:54:17 +0300

    Initial Commit

---
+++ UnitConverter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UnitConverter
+//
+//  Created by Emre Ilhan on 19.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
